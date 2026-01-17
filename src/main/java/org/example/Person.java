f086ecd4b47c6c343bd04a469df0b210a06ec6f5
Ömer Özdemir <byozdemir06@gmail.com>
2026-01-17 17:35:13 +0300

    proje tamamlandı

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Person {
+
+    String firstName;
+
+    String lastName;
+
+    int age;
+
+    int salary;
+
+    Boolean isMarried;
+
+    String[] hobbies;
+
+    public Person (String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName, int age, int salary, boolean isMarried, String[] hobbies){
+        this(firstName, lastName, age);
+        this.salary= salary;
+        this.isMarried= isMarried;
+        this.hobbies=hobbies;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public boolean isTeen(){
+        return this.age >= 13 && this.age <= 19;
+    }
+}
